2bf6ad557ca9a3b2fd4fb2fd24b7530ff9d27730
gopi.jtech@gmail.com <color.ui>
2013-04-19 13:09:28 +0530

    Initial commit

---
+++ Contacts/Contacts/Contacts.waModel.js
@@ -0,0 +1,27 @@
+﻿
+guidedModel =// @startlock
+{
+	Company :
+	{
+		methods :
+		{// @endlock
+			returnAlert:function()
+			{// @lock
+				// Add your code here
+				return "Alert Message From Server!";
+			}// @startlock
+		}
+	},
+	Employee :
+	{
+		fullName :
+		{
+			onGet:function()
+			{// @endlock
+				// Add your code here
+				return this.firstName + '' + this.lastName
+			}// @startlock
+		}
+	}
+};// @endlock
+
---
+++ Contacts/Contacts/WebFolder/Alert.waPage/scripts/index.js
@@ -0,0 +1,31 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+	var button1 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		alert("Message from client");
+	};// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		ds.Company.returnAlert({
+			onSuccess: function(event){
+				alert (event.result);
+				}
+			});
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("button1", "click", button1.click, "WAF");
+// @endregion
+};// @endlock
